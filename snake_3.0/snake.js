38f3481011830860dcb9f09482a5e777e03fc581
Vladislav Artemyev <fasikcool@gmail.com>
2014-03-12 17:38:47 +0400

    1

--- snake_3.0/snake.js
+++ snake_3.0/snake.js
@@ -1,12 +1,16 @@
 $(function() {
 
+  var speed = 500;
+  var snake = [];
+  var l = 0;
+
   function fieldGenerate() {
     var x = 1;
     var y = 1;
     for( ;y <= 35; ) {
       $(".field").append('<div class="block" id="'+ x +'-'+ y +'"></div>');
       x++;
-      if(x == 36) {
+      if(x > 35) {
         y++;
         x = 1;
       }
@@ -24,12 +28,78 @@ $(function() {
   }
 
   function snakeGenerate() {
-    var x = parseInt(Math.random() * (1, 35) + 1);
-    var y = parseInt(Math.random() * (1, 35) + 1);
+    var x = parseInt(Math.random() * (5, 30) + 1);
+    var y = parseInt(Math.random() * (5, 30) + 1);
     $("#"+ x +"-"+ y +"").addClass("snake");
   }
 
+  function snakeAdd() {
+    snake[l + 1] = snake[l];
+    $(snake[l + 1]).addClass("snake");
+    l += 1;
+  }
+
+
+  function rightMove() {
+    var y = getY();
+    var x = getX();
+    getId();
+    $("#"+x+"-"+y+"").removeClass("snake")
+    x += 1;
+    $("#"+x+"-"+y+"").addClass("snake")
+    console.log(x, y);
+  }
+
+  function leftMove() {
+    var y = getY();
+    var x = getX();
+    $("#"+x+"-"+y+"").removeClass("snake")
+    x -= 1;
+    $("#"+x+"-"+y+"").addClass("snake")
+    console.log(x, y);
+  }
+
+  function upMove() {
+    var y = getY();
+    var x = getX();
+    $("#"+x+"-"+y+"").removeClass("snake")
+    y -= 1;
+    $("#"+x+"-"+y+"").addClass("snake")
+    console.log(x, y);
+  }
+
+  function downMove() {
+    var y = getY();
+    var x = getX();
+    $("#"+x+"-"+y+"").removeClass("snake")
+    y += 1;
+    $("#"+x+"-"+y+"").addClass("snake")
+    console.log(x, y);
+  }
+
+  var s39 = 0;
+  var s40 = 0;
+  var s37 = 0;
+  var s38 = 0;
+
+  function clearQueue() {
+    clearInterval(s39);
+    clearInterval(s40);
+    clearInterval(s37);
+    clearInterval(s38);
+  }
+
+  $(document).keydown(function(event) {
+    switch (event.which) {
+      case 39: clearQueue(); s39 = setInterval(rightMove, speed); break;
+      case 40: clearQueue(); s40 = setInterval(downMove, speed); break;
+      case 37: clearQueue(); s37 = setInterval(leftMove, speed); break;
+      case 38: clearQueue(); s38 = setInterval(upMove, speed);
+    }
+  });
+
   fieldGenerate();
   snakeGenerate();
+  snakeAdd();
 
 });
