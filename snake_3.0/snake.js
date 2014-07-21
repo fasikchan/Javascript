12e37f73d00d2982d7fe61534d2c6fd196f13dbe
Vladislav Artemyev <fasikcool@gmail.com>
2014-07-21 18:35:20 +0400

    Продолжение разработки

--- snake_3.0/snake.js
+++ snake_3.0/snake.js
@@ -1,11 +1,11 @@
 var Field = {
-  x: 50,
-  y: 50,
+  x: 25,
+  y: 25,
 
   generate: function() {
     if ($(".field").children()[0] == null) {
       for(var i = 0, x = 1, y = 1; i < this.x * this.y; i++) {
-        if (x > 50) {
+        if (x > 25) {
           x = 1;
           y++;
         };
@@ -14,13 +14,29 @@ var Field = {
       }
     }
   },
+  exploration: function(next_block) {
+    if (next_block == null || next_block.hasClass('snake')) {
+      alert('You lose!');
+    } else {
+      block = $('#'+ Snake.get[Snake.get.length - 1])
+      block.removeClass('snake');
+      Snake.get[Snake.get.length - 1] = Snake.get[0]
+      Snake.get[0] = next_block.attr('id');
+      next_block.addClass('snake');
+    }
+    if (next_block.hasClass('prey')) {
+      next_block.removeClass('prey')
+      Snake.get.unshift(next_block.attr('id'))
+      next_block.addClass('snake')
+    }
+  }
 }
 
 var Snake = {
   get: [],
 
   create: function() {
-    var block = $("#" + parseInt(Math.random() * 51) + "x" + parseInt(Math.random() * 51) + "y")
+    var block = $("#5x5y")
     block.addClass('snake');
     this.get.push(block.attr("id"));
   },
@@ -37,12 +53,30 @@ var Snake = {
 
   moving: {
     left: function() {
-
+      next_block = (Snake.x() - 1) == 0 ? null : $('#' + (Snake.x() - 1) + 'x' + Snake.y() + 'y');
+      Field.exploration(next_block);
     },
+    right: function() {
+      next_block = (Snake.x() + 1) == (Field.x + 1) ? null : $('#' + (Snake.x() + 1) + 'x' + Snake.y() + 'y');
+      Field.exploration(next_block);
+    },
+    up: function() {
+      next_block = (Snake.y() - 1) == 0 ? null : $('#' + Snake.x() + 'x' + (Snake.y() - 1) + 'y');
+      Field.exploration(next_block);
+    },
+    down: function() {
+      next_block = (Snake.y() + 1) == (Field.y + 1) ? null : $('#' + Snake.x() + 'x' + (Snake.y() + 1) + 'y');
+      Field.exploration(next_block);
+    }
   },
 }
 
-
+var Prey = {
+  generate: function() {
+    var block = $("#" + parseInt(Math.random() * 26) + "x" + parseInt(Math.random() * 26) + "y")
+    block.addClass('prey');
+  }
+}
 
 $(function() {
   Field.generate();
