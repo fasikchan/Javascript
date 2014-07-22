310efd2e8999911a6e51ccffd5a4f65e17b9d481
Vladislav Artemyev <fasikcool@gmail.com>
2014-07-22 11:02:44 +0400

    Finish

--- snake_3.0/snake.js
+++ snake_3.0/snake.js
@@ -1,8 +1,10 @@
 var Field = {
   x: 25,
   y: 25,
+  speed: 100,
 
   generate: function() {
+    $(".message").html("<span>Score: "+ (Snake.get.length) +"</span>")
     if ($(".field").children()[0] == null) {
       for(var i = 0, x = 1, y = 1; i < this.x * this.y; i++) {
         if (x > 25) {
@@ -17,17 +19,23 @@ var Field = {
   exploration: function(next_block) {
     if (next_block == null || next_block.hasClass('snake')) {
       alert('You lose!');
+      location.reload();
     } else {
-      block = $('#'+ Snake.get[Snake.get.length - 1])
+      length = Snake.get.length - 1
+      block = $('#'+ Snake.get[length])
       block.removeClass('snake');
-      Snake.get[Snake.get.length - 1] = Snake.get[0]
+      for (i = length; i > 0; i--) {
+        Snake.get[i] = Snake.get[i - 1]
+      }
       Snake.get[0] = next_block.attr('id');
       next_block.addClass('snake');
     }
     if (next_block.hasClass('prey')) {
-      next_block.removeClass('prey')
-      Snake.get.unshift(next_block.attr('id'))
-      next_block.addClass('snake')
+      next_block.removeClass('prey');
+      Snake.get.unshift(next_block.attr('id'));
+      next_block.addClass('snake');
+      $(".message").html("<span>Score: "+ (Snake.get.length - 1) +"</span>")
+      Prey.generate();
     }
   }
 }
@@ -73,12 +81,40 @@ var Snake = {
 
 var Prey = {
   generate: function() {
-    var block = $("#" + parseInt(Math.random() * 26) + "x" + parseInt(Math.random() * 26) + "y")
-    block.addClass('prey');
+    var block = $("#" + (parseInt(Math.random() * 25) + 1) + "x" + (parseInt(Math.random() * 25) + 1) + "y");
+    if(!block.hasClass('snake')) {
+      block.addClass('prey');
+    } else {
+      this.generate();
+    }
   }
 }
 
 $(function() {
+
   Field.generate();
+  Prey.generate();
+  Snake.create();
+
+  var s39 = 0;
+  var s40 = 0;
+  var s37 = 0;
+  var s38 = 0;
+
+  function clearQueue() {
+    clearInterval(s39);
+    clearInterval(s40);
+    clearInterval(s37);
+    clearInterval(s38);
+  }
+
+  $(document).keydown(function(event) {
+    switch (event.which) {
+      case 39: clearQueue(); s39 = setInterval(Snake.moving.right, Field.speed); break;
+      case 40: clearQueue(); s40 = setInterval(Snake.moving.down, Field.speed); break;
+      case 37: clearQueue(); s37 = setInterval(Snake.moving.left, Field.speed); break;
+      case 38: clearQueue(); s38 = setInterval(Snake.moving.up, Field.speed);
+    }
+  });
 
 });
